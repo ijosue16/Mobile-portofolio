fd8f65c48125bf8d9c1a7c9e497b68e63f8e7a60
ijosue16 <iradukundajosue16@gmail.com>
2022-11-22 09:44:36 +0200

    aded javas.file

---
+++ javas.js
@@ -0,0 +1,22 @@
+const hamburgerIcon = document.querySelector('.nav-icon');
+const hamburgerIconCross = document.querySelectorAll('.bar2');
+const navMenuMobile = document.querySelector('.desktop-nav-container');
+const pageName = document.querySelector('.pagename');
+const navHeader = document.querySelector('.mobile-navhead');
+const mobileNav = document.querySelector('.nav-bar');
+
+hamburgerIcon.addEventListener('click', () => {
+  hamburgerIcon.classList.toggle('active');
+  navMenuMobile.classList.toggle('active');
+  pageName.classList.toggle('active');
+  navHeader.classList.toggle('active');
+  mobileNav.classList.toggle('active');
+  hamburgerIconCross.classList.toggle('active');
+});
+document.querySelectorAll('.nav-link').forEach(n=>n.addEventListener("click",()=>{
+    hamburgerIcon.classList.remove('active');
+    navMenuMobile.classList.remove('active');
+    pageName.classList.remove('active');
+    mobileNav.classList.remove('active');
+    hamburgerIconCross.classList.remove('active');
+}))
